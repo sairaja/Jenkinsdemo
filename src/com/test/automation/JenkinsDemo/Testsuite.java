5cb5ec2d2599bca22bee769e71cee13b05febb62
sai <sairaja503@gmail.com>
2014-11-24 23:05:42 +0530

    Third commit
    Updated testsuites

--- src/com/test/automation/JenkinsDemo/Testsuite.java
+++ src/com/test/automation/JenkinsDemo/Testsuite.java
@@ -3,9 +3,6 @@ package com.test.automation.JenkinsDemo;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
-	AmazonAutomationTest.class,
-	FacebookAutomation.class,GmailAutomation.class,Linkedln.class
-})
+@Suite.SuiteClasses({AmazonAutomationTest.class,FacebookAutomation.class,GmailAutomation.class,Linkedln.class})
 public class Testsuite {   
 }  	
